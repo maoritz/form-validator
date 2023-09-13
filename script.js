b7c1f273b04b9a05137e89ac6ad5d09b83b8dd0c
Maor Attias <maorattias17@gmail.com>
2023-09-14 02:19:13 +0300

    refactor if else statements part 1. check required fields function

--- script.js
+++ script.js
@@ -4,39 +4,48 @@ const username = document.getElementById('username');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+// Show Error outline
 function showError(input,message){
     const formControl = input.parentElement
     formControl.className = 'form-control error'
-    const small = document.querySelector('small')
+    const small = formControl.querySelector('small')
     small.innerText = message
 }
 
+// Show Success outline
 function showSuccess(input){
     const formControl = input.parentElement
     formControl.className = 'form-control success'
- 
 }
 
+// Check email is valid
+function validateEmail (email) {
+    const re =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    return re.test(String(email).toLocaleLowerCase())
+  };
+
+// Check required fields
+function checkRequired(inputArr){
+    inputArr.forEach(input => {
+        if(input.value.trim() === ''){
+            showError(input,'is required')
+        } else {
+            showSuccess(input)
+        }
+    })
+}
+
+// Event listeners
 form.addEventListener('submit',function(e){
     e.preventDefault()
-    if(username.value === '') showError(username,'username is required')
-    else showSuccess(username)
+
+    checkRequired([username,email,password,password2])
 })
 
-form.addEventListener('submit',function(e){
-    e.preventDefault()
-    if(email.value === '') showError(email,'email is required')
-    else showSuccess(email)
-})
 
-form.addEventListener('submit',function(e){
-    e.preventDefault()
-    if(password.value === '') showError(password,'password is required')
-    else showSuccess(password)
-})
 
-form.addEventListener('submit',function(e){
-    e.preventDefault()
-    if(password2.value === '') showError(password2,'password2 is required')
-    else showSuccess(password2)
-})
+//Create array of html's inputs names
+// const nodeList = document.querySelectorAll('input')
+// const nodeListToArray = Array.from(nodeList)
+// const inputArr = (() => nodeListToArray.map(item => item.id))()
